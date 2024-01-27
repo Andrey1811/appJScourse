d70d4fea48380823c1ca212020c55730d2321f40
Andrey Korin <andrej-korin@mail.ru>
2024-01-27 16:01:04 +1000

    sqwe

---
+++ 00-js-basic/inform.js
@@ -0,0 +1,19 @@
+// Loop (basics) понятие цикла
+
+
+const numbers = [1, 2, 32, 54, 85];
+// let index = 0
+// while(index < numbers.length) {
+//     console.log(numbers[index] * 2)
+//     index++;
+// }
+
+// console.log('Hello after while')
+
+
+//  for 
+ for(let i = 0; i < numbers.length; i++) {
+numbers[i] *=2
+console.log(numbers[i])
+ }
+ console.log('end for loop')
